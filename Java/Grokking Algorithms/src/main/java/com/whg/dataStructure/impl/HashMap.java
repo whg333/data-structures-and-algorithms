f91c52d9303b1ac18bdaa73b023a0ce3f6ac994b
wanghonggang <wanghonggang@hoolai.com>
2018-09-27 20:59:52 +0800

    update

---
+++ Java/Grokking Algorithms/src/main/java/com/whg/dataStructure/impl/HashMap.java
@@ -0,0 +1,63 @@
+package com.whg.dataStructure.impl;
+
+import com.whg.dataStructure.Collection;
+import com.whg.dataStructure.Map;
+import com.whg.dataStructure.Set;
+
+public class HashMap<K, V> implements Map<K, V> {
+
+    @Override
+    public boolean put(K k, V v) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean remove(K k) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public V get(K k) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean hasKey(K k) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean hasValue(V v) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Set<K> keys() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Collection<V> values() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
